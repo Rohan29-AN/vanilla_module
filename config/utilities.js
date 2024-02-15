9ebe8b95a76aefd45fbab7e14c58dd412fd1b51f
Rohan <rohanrc101@gmail.com>
2024-02-15 13:48:12 +0300

    [feat] hash function

--- config/utilities.js
+++ config/utilities.js
@@ -1,10 +1,13 @@
-module.exports={
+var crypto = require('crypto');
+module.exports = {
     /**
-     * 
-     * @param {*} secret 
-     * @param {*} payload 
+     * Hashes the payload using SHA256 algorithm and a secret key.
+     * @param {string} secret - The secret key used for hashing.
+     * @param {string} payload - The value to be hashed.
+     * @returns {string} - The hashed value.
      */
-    hashData(secret,payload){
-        console.log("Test")
+    hashData(secret, payload) {
+        const hashedData = crypto.createHmac('SHA256', secret).update(payload).digest('hex')
+        return hashedData.toUpperCase()
     }
 }
